88f05c28e715deb5b4d17389c52b6044be092525
Nozomi-Hijikata <nozomi.hijikata@techouse.jp>
2024-02-29 23:35:51 +0900

    ✨initial commit

---
+++ lib/ycv.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "ycv/version"
+
+module Ycv
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/ycv/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Ycv
+  VERSION = "0.1.0"
+end
