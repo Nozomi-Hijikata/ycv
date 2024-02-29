0f1a1da397e925a63e7da049746fe6f719107061
Nozomi-Hijikata <nozomi.hijikata@techouse.jp>
2024-03-01 01:26:57 +0900

    fix: 正規表現のバグを修正

--- lib/ycv/to_csv.rb
+++ lib/ycv/to_csv.rb
@@ -6,7 +6,7 @@ require "yaml"
 module Ycv
   class ToCsv
     def self.call(file_path)
-      file_path[/(\w+).yml$/, 1]
+      file_path[/(\w+)\.(yml|yaml)$/, 1]
       fixtures = YAML.load_file(file_path)
       headers = fixtures.first[1].keys
       CSV.generate do |c|
